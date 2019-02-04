6132cb82fa18e5460754b17ec67b7bcda7d8e481
Phillipe Jórdan <phillipe.j@gmail.com>
2019-02-04 01:43:45 -0200

    Initial Commit

---
+++ dice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dice
+//
+//  Created by Phillipe Osorio on 04/02/19.
+//  Copyright © 2019 Phillipe Osorio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
